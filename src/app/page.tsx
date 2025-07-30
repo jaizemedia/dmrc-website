fdaba6557b8742f71b9680664fc21ca7d4644251
jaizemedia <info@jaizemedia.com>
2025-07-30 11:18:37 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import Container from "@/components/Container";
-import Section from "@/components/Section";
 import CTA from "@/components/CTA";
 
 const HomePage: React.FC = () => {
