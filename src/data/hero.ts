6ab2cb6ab5da5027f786f0781042df6be12e8c2f
jaizemedia <info@jaizemedia.com>
2025-07-30 16:30:27 +0000

    , milton keynes change

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Be a part of our church family!',
-    subheading: 'Join us every Sunday at 9am @Great Holm Community Centre',
+    subheading: 'Join us every Sunday at 9am @Great Holm Community Centre, Milton Keynes',
     centerImageSrc: '/images/hero-mockup.png',
 }
