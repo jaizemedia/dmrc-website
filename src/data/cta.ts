0ad0dc2fa1c2bb0d90e73a3ebefa32d26db1d9b0
jaizemedia <info@jaizemedia.com>
2025-07-30 10:53:54 +0000

    add appsore thing

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -1,4 +1,6 @@
 export const ctaDetails = {
     heading: 'Join us Every Sunday at 9am',
     subheading: 'Great Holm Community Centre, 87 Kensington Drive, Milton Keynes, MK8 9AY',
+    appStoreUrl: '#',
+    googlePlayUrl: '#'
 }
