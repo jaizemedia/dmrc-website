8d710c4641feed9c13c3d6149b3b2b347e465505
jaizemedia <info@jaizemedia.com>
2025-07-30 12:36:41 +0000

    Giving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Container from "@/components/Container";
 import CTA from "@/components/CTA";
 import React from "react";
 import About from "@/components/About";
+import Give from "@/components/Give";
 
 const HomePage: React.FC = () => {
   return (
     <>
-      
+      <Give/>
       <Hero />
       <About />
       <Container>
