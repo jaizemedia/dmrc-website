25e4542e1fd38e5d61df4110940a7ea379db9887
jaizemedia <info@jaizemedia.com>
2025-07-30 10:59:03 +0100

    Update hero.ts

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'We are a family, not just a gathering.',
     subheading: 'Join us every Sunday at 9am @Great Holm Community Centre',
-    centerImageSrc: '/images/hero-mockup.webp',
+    centerImageSrc: '/images/Untitled design (13).png',
 }
