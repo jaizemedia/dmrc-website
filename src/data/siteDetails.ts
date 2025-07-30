f7b53c69e6a21318f7752bb7800778a4399f7217
jaizemedia <info@jaizemedia.com>
2025-07-30 10:32:10 +0100

    Update siteDetails.ts

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,12 +1,12 @@
 export const siteDetails = {
-    siteName: 'Finwise',
-    siteUrl: 'https://finwise-omega.vercel.app/',
+    siteName: 'Disciple Makers Revival Church',
+    siteUrl: 'https://www.disciplemakerschurch.uk/',
     metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
-        description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
+        title: 'Disciple Makers Revival Church',
+        description: 'We exist not just for ourselves, but to reach the world with the good news of Jesus. - Every member is a missionary — in homes, workplaces, and the nations.',
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
+    siteLogo: `${process.env.BASE_PATH || ''}/images/Church Logo (1).png`, // or use a string for the logo e.g. "TechStartup"
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
 }
