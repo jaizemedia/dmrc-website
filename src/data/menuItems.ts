dffa1b8e0bd7066d603acbe823620140358f7e04
jaizemedia <info@jaizemedia.com>
2025-07-30 12:01:08 +0000

    menu menuItems

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -2,6 +2,10 @@ import { IMenuItem } from "@/types";
 
 export const menuItems: IMenuItem[] = [
     {
+    text: "About",
+    url: "#about"
+  },
+  {
             text: "YouTube",
             url: "https://www.youtube.com/@disciplemakerschurch_mk"
         },
@@ -12,5 +16,10 @@ export const menuItems: IMenuItem[] = [
         {
             text: "TikTok",
             url: "https://www.tiktok.com/@disciplemakerschurchmk"
+        },
+        {
+            text: "Facebook",
+            url: "https://www.facebook.com/share/1BuWahagTw/?mibextid=wwXlfr"
         }
+        
 ];
