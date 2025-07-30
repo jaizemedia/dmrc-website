01be3ad86a2ad6fe71c37a251a8fa4836e8b3d17
jaizemedia <info@jaizemedia.com>
2025-07-30 10:28:06 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC = () => {
                 <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
                     {/* Logo */}
                     <Link href="/" className="flex items-center gap-2">
-                        <FaFingerprint className="text-foreground min-w-fit w-7 h-7" />
+                        <img src="/images/Church Logo (1).png" alt="Church Logo" className="w-7 h-7 object-contain" />
                         <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
                             {siteDetails.siteName}
                         </span>
