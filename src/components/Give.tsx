1f98a8d4736579e975426bc43959f43e641d24e4
jaizemedia <info@jaizemedia.com>
2025-07-30 12:42:51 +0000

    give size made smaller

--- src/components/Give.tsx
+++ src/components/Give.tsx
@@ -1,8 +1,8 @@
-const CTA: React.FC = () => {
+const Give: React.FC = () => {
     return (
-        <section id="give" className="mt-10 mb-5 lg:my-20">
+        <section id="give"   className="mt-10 mb-5 lg:my-20 max-h-[600px] overflow-hidden">
             <div className="relative h-full w-full z-10 mx-auto py-12 sm:py-20">
-                <div className="h-full w-full"
+                <div className="h-full w-full">
                     <div className="rounded-3xl opacity-95 absolute inset-0 -z-10 h-full w-full bg-[#050a02] bg-[linear-gradient(to_right,#12170f_1px,transparent_1px),linear-gradient(to_bottom,#12170f_1px,transparent_1px)] bg-[size:6rem_4rem]">
                         <div className="rounded-3xl absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_600px_at_50%_500px,#1C1C02,transparent)]"></div>
                     </div>
@@ -26,4 +26,4 @@ const CTA: React.FC = () => {
     )
 }
 
-export default CTA
+export default Give
